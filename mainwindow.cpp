e3f26df4a424603ca2563cb8fc072cf23409fdc4
Jan M. Binder <jan.binder@sfz-bw.de>
2011-07-28 13:50:29 +0200

    Integrieren von ListController sowie diverse Verbesserungen

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "themeclock.h"
+#include "listcontroller.h"
 #include <QtCore/QTimer>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -8,6 +9,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->graphicsView->setRenderHint(QPainter::Antialiasing);
+    ui->graphicsView->setRenderHint(QPainter::SmoothPixmapTransform);
+
 
     QTimer *timer = new QTimer();
     connect(timer, SIGNAL(timeout()), ui->graphicsView, SLOT(act()));
@@ -20,11 +25,20 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(thc, SIGNAL(started(int)), ui->graphicsView, SLOT(setTime(int)));
     connect(thc, SIGNAL(paused(int)), ui->graphicsView, SLOT(setTime(int)));
     connect(thc, SIGNAL(restarted(int)), ui->graphicsView, SLOT(setTime(int)));
-
-    thc->setAllowedTime(10000);
+    connect(thc, SIGNAL(stopped(int)), ui->graphicsView, SLOT(setTime(int)));
 
     connect(ui->startstop, SIGNAL(clicked()), thc, SLOT(startorpause()));
     connect(thc, SIGNAL(timeUpdate(QString)), ui->lcdNumber, SLOT(display(QString)));
+
+    ListController *lc = new ListController();
+
+    connect(ui->ffwd, SIGNAL(clicked()), lc, SLOT(forward()));
+    connect(ui->bwd, SIGNAL(clicked()), lc, SLOT(backward()));
+    connect(lc, SIGNAL(newAllowedTime(int)), thc, SLOT(setAllowedTime(int)));
+    connect(lc, SIGNAL(resetTime()), thc, SLOT(reset()));
+
+    thc->setAllowedTime(20000);
+
 }
 
 MainWindow::~MainWindow()
