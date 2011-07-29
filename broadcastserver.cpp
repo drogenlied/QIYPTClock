3ca3b57e4a701d3819dc17f0299763b2108728cc
Jan M. Binder <jan.binder@sfz-bw.de>
2011-07-29 22:37:39 +0200

    implement broadcastserver

--- broadcastserver.cpp
+++ broadcastserver.cpp
@@ -1,9 +1,18 @@
 #include "broadcastserver.h"
+#include <QDataStream>
+#include <QtNetwork>
+#include <QDataStream>
 
-BroadcastServer::BroadcastServer(QObject *parent) :
+BroadcastServer::BroadcastServer(QObject *parent, unsigned int p) :
     QObject(parent)
 {
     udpSocket = new QUdpSocket(this);
+    if (p < INT_MAX) {
+        port = p;
+    }else{
+        port = p/2;
+    }
+    connect(this, SIGNAL(updated()), this, SLOT(broadcast()));
 }
 
 BroadcastServer::~BroadcastServer(){
@@ -11,17 +20,34 @@ BroadcastServer::~BroadcastServer(){
 }
 
 void BroadcastServer::setStageName(QString name){
-
+    stagename = name;
+    emit updated();
 }
 
 void BroadcastServer::setAllowedTime(int t){
-
+    if (t>0)
+        allowedtime = t;
+    emit updated();
 }
 
 void BroadcastServer::updateTime(int t){
-
+    time = t;
+    emit updated();
 }
 
-void BroadcastServer::setBroadcastPort(int p){
+void BroadcastServer::setBroadcastPort(unsigned int p){
+    if (p>0)
+        port = p;
+    emit updated();
+}
 
+void BroadcastServer::broadcast(){
+    QByteArray datagram;
+    QDataStream dgstream(&datagram, QIODevice::ReadWrite);
+    dgstream << (quint32)0xABCDEF;
+    dgstream << (quint32)allowedtime;
+    dgstream << (quint32)time;
+    dgstream << stagename;
+    qDebug("dgram: %d", datagram.size());
+    udpSocket->writeDatagram(datagram.data(), datagram.size(), QHostAddress::Broadcast, port);
 }
--- broadcastserver.h
+++ broadcastserver.h
@@ -8,22 +8,27 @@ class BroadcastServer : public QObject
 {
     Q_OBJECT
 public:
-    explicit BroadcastServer(QObject *parent = 0);
+    explicit BroadcastServer(QObject *parent = 0, unsigned int p = 54545);
     ~BroadcastServer();
 
 signals:
-    void connectionStatus();
+    void socketStatus();
+    void updated();
 
 public slots:
     void setStageName(QString);
     void setAllowedTime(int);
     void updateTime(int);
-    void setBroadcastPort(int);
+    void setBroadcastPort(unsigned int);
+
+private slots:
+    void broadcast();
 
 private:
     QUdpSocket *udpSocket;
     QString stagename;
-    int allowedtime, time, port;
+    int allowedtime, time;
+    unsigned int port;
 };
 
 #endif // BROADCASTSERVER_H
