496b7aa4aeab455905012fef26f6cc75eacc0973
Jan M. Binder <jan.binder@sfz-bw.de>
2011-08-04 13:38:11 +0200

    GPLv3 license headers for everyone

--- themeclockwidget.cpp
+++ themeclockwidget.cpp
@@ -1,3 +1,20 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Jan M. Binder
+** Contact: jan.binder@sfz-bw.de
+**
+** $QT_BEGIN_LICENSE:GPL$
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "themeclockwidget.h"
 #include <cmath>
 
