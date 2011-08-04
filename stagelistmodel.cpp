347c30a8dcf5711b8a2ae2bf787f7fc8467c252f
Jan M. Binder <jan.binder@sfz-bw.de>
2011-08-04 13:29:56 +0200

    Import TableModel from Qt examples and adapt

---
+++ stagelistmodel.cpp
@@ -0,0 +1,145 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+ ** Contact: Qt Software Information (qt-info@nokia.com)
+ ** Copyright (C) 2011 Jan M. Binder
+ ** Contact: jan.binder@sfz-bw.de
+ **
+ ** $QT_BEGIN_LICENSE:GPL$
+ ** GNU General Public License Usage
+ ** This file may be used under the terms of the GNU
+ ** General Public License version 3.0 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.GPL included in the
+ ** packaging of this file.  Please review the following information to
+ ** ensure the GNU General Public License version 3.0 requirements will be
+ ** met: http://www.gnu.org/copyleft/gpl.html.
+ ** $QT_END_LICENSE$
+ **
+ ****************************************************************************/
+
+#include "stagelistmodel.h"
+
+StageListModel::StageListModel(QObject *parent)
+     : QAbstractTableModel(parent)
+ {
+ }
+
+ StageListModel::StageListModel(QList< QPair<int, QString> > pairs, QObject *parent)
+     : QAbstractTableModel(parent)
+ {
+     listOfStages=pairs;
+ }
+
+ int StageListModel::rowCount(const QModelIndex &parent) const
+ {
+     Q_UNUSED(parent);
+     return listOfStages.size();
+ }
+
+ int StageListModel::columnCount(const QModelIndex &parent) const
+ {
+     Q_UNUSED(parent);
+     return 2;
+ }
+
+ QVariant StageListModel::data(const QModelIndex &index, int role) const
+ {
+     if (!index.isValid())
+         return QVariant();
+
+     if (index.row() >= listOfStages.size() || index.row() < 0)
+         return QVariant();
+
+     if (role == Qt::DisplayRole) {
+         QPair<int, QString> pair = listOfStages.at(index.row());
+
+         if (index.column() == 0)
+             return pair.first;
+         else if (index.column() == 1)
+             return pair.second;
+     }
+     return QVariant();
+ }
+
+ QVariant StageListModel::headerData(int section, Qt::Orientation orientation, int role) const
+ {
+     if (role != Qt::DisplayRole)
+         return QVariant();
+
+     if (orientation == Qt::Horizontal) {
+         switch (section) {
+             case 0:
+                 return tr("Duration");
+
+             case 1:
+                 return tr("Stage Name");
+
+             default:
+                 return QVariant();
+         }
+     }
+     return QVariant();
+ }
+
+ bool StageListModel::insertRows(int position, int rows, const QModelIndex &index)
+ {
+     Q_UNUSED(index);
+     beginInsertRows(QModelIndex(), position, position+rows-1);
+
+     for (int row=0; row < rows; row++) {
+         QPair<int, QString> pair(1, " ");
+         listOfStages.insert(position, pair);
+     }
+
+     endInsertRows();
+     return true;
+ }
+
+ bool StageListModel::removeRows(int position, int rows, const QModelIndex &index)
+ {
+     Q_UNUSED(index);
+     beginRemoveRows(QModelIndex(), position, position+rows-1);
+
+     for (int row=0; row < rows; ++row) {
+         listOfStages.removeAt(position);
+     }
+
+     endRemoveRows();
+     return true;
+ }
+
+ bool StageListModel::setData(const QModelIndex &index, const QVariant &value, int role)
+ {
+         if (index.isValid() && role == Qt::EditRole) {
+                 int row = index.row();
+
+                 QPair<int, QString> p = listOfStages.value(row);
+
+                 if (index.column() == 0)
+                         p.first = value.toInt();
+                 else if (index.column() == 1)
+                         p.second = value.toString();
+         else
+             return false;
+
+         listOfStages.replace(row, p);
+                 emit(dataChanged(index, index));
+
+         return true;
+         }
+
+         return false;
+ }
+
+ Qt::ItemFlags StageListModel::flags(const QModelIndex &index) const
+ {
+     if (!index.isValid())
+         return Qt::ItemIsEnabled;
+
+     return QAbstractTableModel::flags(index) | Qt::ItemIsEditable;
+ }
+
+ QList< QPair<int, QString> > StageListModel::getList()
+ {
+     return listOfStages;
+ }
---
+++ stagelistmodel.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Qt Software Information (qt-info@nokia.com)
+** Copyright (C) 2011 Jan M. Binder
+** Contact: jan.binder@sfz-bw.de
+**
+** $QT_BEGIN_LICENSE:GPL$
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef STAGELISTMODEL_H
+#define STAGELISTMODEL_H
+
+#include <QAbstractTableModel>
+#include <QPair>
+#include <QList>
+
+class StageListModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    StageListModel(QObject *parent=0);
+    StageListModel(QList< QPair<int, QString> > listofStages, QObject *parent=0);
+
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role=Qt::EditRole);
+    bool insertRows(int position, int rows, const QModelIndex &index=QModelIndex());
+    bool removeRows(int position, int rows, const QModelIndex &index=QModelIndex());
+    QList< QPair<int, QString> > getList();
+
+private:
+    QList< QPair<int, QString> > listOfStages;
+};
+
+#endif // STAGELISTMODEL_H
